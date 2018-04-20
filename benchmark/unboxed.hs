665d31de3c13f9ba463c5fac700fb8dc46de3edd
Juan Paucar <jpaucar@stackbuilders.com>
2018-04-20 10:48:54 -0500

    Add benchamark results

--- benchmark/unboxed.hs
+++ benchmark/unboxed.hs
@@ -1,18 +1,32 @@
 {-# LANGUAGE MagicHash #-}
 
 import GHC.Prim
+import GHC.Exts
 
 import Criterion.Main
 
-{-import GHC.Exts-}
 
 
-sum :: Int -> Int -> Int
-sum = undefined
+sum' :: Int -> Int -> Int
+sum' = (+)
 
-sum' :: Int# -> Int# -> Int#
-sum' = undefined
+sum'' :: Int# -> Int# -> Int -> Int
+sum'' a b _ = I# (a +# b)
 
-main = defaultMain []
+main = defaultMain [
+        bgroup "sum" [
+          bench "million" $ nf (sum' 1123345) 2123987
+        , bench "billion" $ nf (sum' 1123345478) 2123987192
+        , bench "trillion" $ nf (sum' 1123345478109) 2123987192098
+        , bench "zillion" $ nf (sum' 1611686018427387903) 2611686018427387903
+        ],
+
+        bgroup "sum primitive" [
+          bench "million" $ nf (sum'' 1123345# 2123987#) 1
+        , bench "billion" $ nf (sum'' 1123345478# 2123987192#) 1
+        , bench "trillion" $ nf (sum'' 1123345478109# 2123987192098#) 1
+        , bench "zillion" $ nf (sum'' 1611686018427387903# 2611686018427387903#) 1
+        ]
+       ]
 
 
