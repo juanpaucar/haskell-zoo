210e0cabe847035da2a5767e8d81ea07c5e011b4
Juan Paucar <jpaucar@stackbuilders.com>
2018-04-27 14:54:09 -0500

    correct monoid implementation

--- src/MonoidAndSemigroup/Sample.hs
+++ src/MonoidAndSemigroup/Sample.hs
@@ -7,13 +7,8 @@ import Basic.Sample (MyList(..))
 
 instance Monoid (MyList a) where
   mempty = Nil
-  mappend Nil Nil = Nil
-  mappend cons Nil = cons
   mappend Nil cons = cons
-  mappend (Cons x xs) (Cons y ys) = (Cons x Nil) `mappend`
-                                    xs           `mappend`
-                                    (Cons y Nil) `mappend`
-                                    ys
+  mappend (Cons x xs) cons = Cons x (xs `mappend` cons)
 
 instance Semigroup (MyList a) where
   (<>) = mappend
