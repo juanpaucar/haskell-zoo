d3863b2110ba3b313c045c81c17ebe91c6e59f9a
Juan Paucar <jpaucar@stackbuilders.com>
2018-06-08 15:06:53 -0500

    monad eval

--- src/Main.hs
+++ src/Main.hs
@@ -4,8 +4,10 @@ import Control.Monad (void)
 
 import qualified Basic.Sample as Basic
 import qualified ParMonad.Sample as Par
+import qualified Eval.Sample as Eval
 
 main :: IO ()
 main = do
   -- Basic.sample
-  print $ Par.fib 40
+  -- print $ Par.fib 40
+  print $ Eval.fibo 50
